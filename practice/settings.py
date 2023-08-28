eac0e7eb4e126c63f503f308335abf7bb62953f7
Thirdy <65775625+thirdygayares@users.noreply.github.com>
2023-08-29 02:30:47 +0800

    ready files for vercel

--- practice/settings.py
+++ practice/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ja^8s6&c)tm1d-6u)j4+qlfm_&ory&qn@1@cqupvl6awxcctb0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
